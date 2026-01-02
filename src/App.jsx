16dfc0ce3f8b09ca1bd44937d117cdded3b9a0e6
Johann George <johanngeorge2003@gmail.com>
2026-01-02 18:22:50 +0530

    Implemented Login

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import { Outlet } from 'react-router-dom'
+import Register from './pages/Register.jsx'
 import Home from './pages/Home.jsx'
 
 function App() {
